1b0173a40a4c5944bfd2248e9548b0f8edf51738
Liam Mills <liam.mills1511@gmail.com>
2021-01-19 17:48:12 +1100

    eslint fixes

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NextButton = ({ data }) => (
-  <button type="submit" className="nextButton" onPress={() => console.log(data)}>
+  <button type="submit" className="nextButton">
     <h3>Next</h3>
+    <h5>{data}</h5>
   </button>
 );
 
